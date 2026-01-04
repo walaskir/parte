c3639069315e75b7320704568af233dca7640571
Robert Walaski <robert@walaski.cz>
2026-01-04 15:12:36 +0100

    Limit parte download schedule to weekdays only

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-// Scheduler: stahování parte každý den v 16:00
+// Scheduler: stahování parte v pracovní dny (Po-Pá) v 16:00
 Schedule::command('parte:download --all')
     ->dailyAt('16:00')
+    ->weekdays()
     ->name('download-death-notices')
     ->onOneServer()
     ->withoutOverlapping();
