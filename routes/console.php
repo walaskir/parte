46acf1b6b46fa51e36aa466711552960f060ba40
Robert Walaski <robert@walaski.cz>
2026-01-08 11:53:09 +0100

    feat(scheduler): add automatic queue processing after download in local env

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
 // Scheduler: stahování parte v pracovní dny (Po-Pá) v 16:00
@@ -8,4 +9,14 @@ Schedule::command('parte:download --all')
     ->weekdays()
     ->name('download-death-notices')
     ->onOneServer()
-    ->withoutOverlapping();
+    ->withoutOverlapping()
+    ->then(function () {
+        // Jednorázové zpracování extraction fronty po stažení (jen v local prostředí)
+        if (app()->environment('local')) {
+            Artisan::call('queue:work', [
+                '--stop-when-empty' => true,
+                '--queue' => 'extraction,default',
+                '--tries' => 3,
+            ]);
+        }
+    });
